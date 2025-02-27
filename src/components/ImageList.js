3daa12f8f128e94944f1e88101777ababeb0174a
Jack Bennett <59575631+Bennettoyo@users.noreply.github.com>
2025-02-27 22:06:54 +0000

    Passing down props to child component

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,5 +1,11 @@
-function ImageList() {
-  return <div>ImageList</div>;
+import ImageShow from "./ImageShow";
+
+function ImageList({ images }) {
+  const renderedImages = images.map((image) => {
+    return <ImageShow image={image} />;
+  });
+
+  return <div>ImageList: {renderedImages}</div>;
 }
 
 export default ImageList;
